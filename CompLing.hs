4c0d9c5110238cf56e6b357294e7bf2d6a70c5fd
otha4350 <90687246+otha4350@users.noreply.github.com>
2021-11-22 07:41:43 +0100

    bättre funktionsspecifikationer + edvins funktioner

--- CompLing.hs
+++ CompLing.hs
@@ -20,7 +20,8 @@ Computes a tally of all the distinct words appearing in the document.
 RETURNS: List of WordTallys, one for each unique word in xs, with the tally of the word.
 EXAMPLES:
   wordCount [["a","b","c","a","b","a"],["d","e"]] == [("a",3),("b",2),("c",1),("d",1),("e",1)]
-
+  wordCount [["a", "rose", "is", "a", "rose"],["but", "so", "is", "a", "rose"]] == 
+    [("a",3),("rose",3),("is",2),("but",1),("so",1)]
 -}
 wordCount :: Document -> WordTally
 wordCount xs = nub [(x, length $ filter (==x) cxs) | x<-cxs]
@@ -28,7 +29,7 @@ wordCount xs = nub [(x, length $ filter (==x) cxs) | x<-cxs]
 
 -----
 {- adjacentPairs xs
-Computes all the pairs in the document sentence-wise.
+Computes all the pairs in each sentence in the document.
 
 RETURNS: Pairs containing all pairs in each sentence in xs.
 EXAMPLES: 
@@ -40,25 +41,27 @@ adjacentPairs :: Document -> Pairs
 adjacentPairs [] = []
 adjacentPairs (x:xs) = adjacentPairsInSentence x ++ adjacentPairs xs
 
-{- adjacentPairsInSentence xs
-Computes all the pairs in a sentence.
+{- adjacentPairsInSentence sent
+Computes all the pairs of words in a sentence.
 
-RETURNS: Pairs of all words in xs.
+RETURNS: Pairs of all words in sent.
 EXAMPLES:
   adjacentPairsInSentence ["time", "for", "a", "break"] == [("time","for"),("for","a"),("a","break")]
   adjacentPairsInSentence ["a"] == []
 -}
 adjacentPairsInSentence :: Sentence -> Pairs
+
+--VARIANT: length sent
 adjacentPairsInSentence [] = []
 adjacentPairsInSentence [x] = []
 adjacentPairsInSentence (x:y:xs) = (x,y) : adjacentPairsInSentence (y:xs)
 
 -----
 
-{- initialPairs xs
-Computes the first pairs of each sentence in a document.
+{- initialPairs doc
+Computes the pair of the first two words of each sentence in a document.
 
-RETURNS: Initial pairs of all sentences in xs.
+RETURNS: Pairs of the first two words of all sentences in doc.
 EXAMPLES:
   initialPairs [["time", "for", "a", "break"], ["not", "yet"]]
 == [("time","for"),("not", "yet")]
@@ -69,13 +72,12 @@ initialPairs [] = []
 initialPairs [x] = []
 initialPairs (x:xs) = head (adjacentPairsInSentence x) : initialPairs xs
 
-{- finalPairs xs
-Computes the first pairs of each sentence in a document.
+{- finalPairs doc
+Computes the pair of the last two words of each sentence in a document.
 
-RETURNS: Initial pairs of all sentences in xs.
+RETURNS: Pairs of the last two words of all sentences in doc.
 EXAMPLES:
-  finalPairs [["time", "for", "a", "break"], ["not", "yet"]]
-== [("a", "break"),("not", "yet")]
+  finalPairs [["time", "for", "a", "break"], ["not", "yet"]] == [("a", "break"),("not", "yet")]
   finalPairs [["a"]] == []
 -}
 finalPairs :: Document -> Pairs
@@ -84,28 +86,61 @@ finalPairs [x] = []
 finalPairs (x:xs) = last (adjacentPairsInSentence x) : finalPairs xs
 
 -----
-
+{- pairsCount xs
+Computes a tally of all pairs, such as those computed by adjacentPairs.
+RETURNS: A list with pairs and their number of occurances as elements.
+Examples:
+  pairsCount [("big","bear"),("bear","big"),("big","dog")] == [(("bear","big"),2),(("big","dog"),1)]
+-}
 pairsCount :: Pairs -> PairsTally
-pairsCount = undefined  -- remove "undefined" and write your function here
+pairsCount xs = nub [(x, length $ filter (==x) sxs) | x<-sxs]
+  where sxs = map sortInTuple xs
+
+{- sortInTuple (a,b)
+Orders a pair of strings in alphabetical order
+Returns: An alphabetically ordered pair of strings.
+EXAMPLES:
+  sortInTuple ("b","a") == ("a","b")
+  sortInTuple ("a","A") == ("A","a")
+-}
+sortInTuple :: (String, String) -> (String, String)
+sortInTuple (a,b)
+  | a < b = (a,b)
+  | otherwise = (b,a)
 
 -----
 
 {- neighbours xs s
-Gives all the words that appear with a certain word and their tallies.
+Computes all the words that appear next to a certain word, and the number of times they appear.
 
-RETURNS: WordTally of all words that are in a pair with s and the tally of the pair.
+RETURNS: WordTally of all words that are in a pair with s in xs and the tally of the given pair.
 EXAMPLES:
   neighbours [(("bear","big"),2),(("big","dog"),1)] "big" == [("bear",2),("dog",1)]
   neighbours [(("bear","big"),1)] "other" == []
   neighbours [] "other" == []
 -}
 neighbours :: PairsTally -> String -> WordTally
-neighbours xs s = [if a==s then (b,c) else (a,c) | ((a,b),c)<-xs, a==s || b==s]
+neighbours xs s = [if a==s 
+                    then (b,c) 
+                    else (a,c) 
+                  | ((a,b),c)<-xs, a==s || b==s]
 
 -----
 
+{-mostCommonNeighbour xs s
+Computes the word that occurs next to a certain word the most.
+
+RETURNS: Nothing if s is not in any of the pairs in xs, or Just the most common word to occur in a pair with s.
+EXAMPLES: 
+  mostCommonNeighbour [(("bear","big"),2),(("big","dog"),1)] "big" == Just "bear"
+  mostCommonNeighbour [(("bear","big"),1)] "other" == Nothing
+  mostCommonNeighbour [] "other" == Nothing
+-}
+
 mostCommonNeighbour :: PairsTally -> String -> Maybe String
-mostCommonNeighbour = undefined  -- remove "undefined" and write your function here
+mostCommonNeighbour xs s
+  | null $ neighbours xs s = Nothing
+  | otherwise = Just $ fst (maximumBy (\(_,a) (_,b) -> compare a b) (neighbours xs s))
 
 
 
